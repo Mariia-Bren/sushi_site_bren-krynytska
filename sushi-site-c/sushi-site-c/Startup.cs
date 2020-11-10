dcf64b837a85c27483da4b94d8f091da4397275c
Mariia-Bren <55378867+Mariia-Bren@users.noreply.github.com>
2020-11-10 10:13:07 +0200

    delete project

---
+++ sushi-site-c/sushi-site-c/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(sushi_site_c.Startup))]
+namespace sushi_site_c
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
