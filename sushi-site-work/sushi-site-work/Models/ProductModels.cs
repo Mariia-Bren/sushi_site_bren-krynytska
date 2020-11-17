270c92f600c4c05b851121b02a4d095cc47428b5
Mariia-Bren <55378867+Mariia-Bren@users.noreply.github.com>
2020-11-17 13:56:14 +0200

    new project

---
+++ sushi-site-work/sushi-site-work/Models/ProductModels.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace sushi_site_work.Models
+{
+    public class Category
+    {
+        public Category()
+        {
+            subCategories = new HashSet<SubCategory>();
+        }
+        public int Id { get; set; }
+        [Display(Name="Категорія")]
+        public string Name { get; set; }
+        [Display(Name = "Опис категорії")]
+        public string Description { get; set; }
+        public ICollection<SubCategory> subCategories { get; set; }
+    }
+    public class SubCategory
+    {
+        public SubCategory()
+        {
+            products = new HashSet<Product>();
+        }
+        public int Id { get; set; }
+        [Display(Name = "Підкатегорія")]        
+        public string Name { get; set; }
+        [Display(Name = "Опис підкатегорії")]
+        public string Description { get; set; }
+        [Display(Name = "Категорія")]
+        public int CategoryId { get; set; }
+        public virtual Category Category { get; set; }
+        public ICollection<Product> products { get; set; }
+    }
+    public class Product
+    {
+        public int Id { get; set; }
+        [Display(Name = "Назва")]
+        public string Name { get; set; }
+        [Display(Name = "Опис")]
+        public string Description { get; set; }
+        [Display(Name = "Вартість")]
+        public int Cost { get; set; }
+        [Display(Name = "Фото")]
+        public string PhotoPath { get; set; }
+        [Display(Name = "Підкатегорія")]
+        public int SubCategoryId { get; set; }
+        public virtual SubCategory SubCategory { get; set; }
+    }
+    public class ProductImage
+    {
+        public int Id { get; set; }
+        public int ProductId { get; set; }
+        public string PhotoPath { get; set; }
+    }
+}
